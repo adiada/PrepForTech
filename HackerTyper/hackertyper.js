0b7a705076276fbe43283669c020257b1a3d4fa2
Adharsh Suresh <adharshsuresh1997@gmail.com>
2021-09-14 01:34:46 +0530

    New changes to hackertyper

--- HackerTyper/hackertyper.js
+++ HackerTyper/hackertyper.js
@@ -3,9 +3,10 @@ var lines = "";
 let input = document.querySelector('input[type="file"]');
 let typer = document.getElementById('typefield');
 let cursor = document.getElementById('cursor'); //this element gets changed when the span gets rewritten 
+let access = document.querySelector('div[class="access"]');
 
 let blinkOnOff = 1;
-
+let accBlinkerOnOff = 0;
 
 //fetching the file from directory
 fetch('./merge_sort.txt')
@@ -30,9 +31,10 @@ const blinker = setInterval(function(){
 
 let i = 0;
 let typestring = '';
+let onlyOnce = 1;
 
 document.addEventListener('keydown',(e)=>{
-    if(!lines == "" && i<=length(lines)){
+    if(!lines == "" && i<= lines.length){
         typestring = typer.innerText;
         typestring = typestring.substr(0,typestring.length - 1);
         typestring += lines.substr(i,3);
@@ -41,6 +43,26 @@ document.addEventListener('keydown',(e)=>{
         i = i + 3;
         typer.innerHTML += "<span id=\"cursor\">|</span>"
         cursor = document.getElementById('cursor');
+    }
+    else{
+        if(e.code == "Enter" && onlyOnce ){
+            onlyOnce = 0;
+            access.style.opacity = "1";
+            const accblink = setInterval(function(){
+                if(accBlinkerOnOff){
+                    access.style.fontWeight = 200;
+                    // access.style.fontSize = "1em"
+                    access.style.border = "1px solid rgb(31, 209, 31)";
+                    accBlinkerOnOff = 0;
+                }
+                else{
+                    access.style.fontWeight = 700;
+                    // access.style.fontSize = "1.1em";
+                    access.style.border = "1.5px solid rgb(31, 209, 31)";
+                    accBlinkerOnOff = 1;
+                }
+            },1000);
+        }
     }
 });
 
